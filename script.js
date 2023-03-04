e42846ed68fd6b25dfc1ee08ecff16e92ac09393
albertodiaz <carlosalberto.diazjimenez@gmail.com>
2023-03-04 12:36:05 -0500

    Created Iniciated

---
+++ script.js
@@ -0,0 +1,7 @@
+const txtBtn = document.getElementById("txtBtn");
+const submitBtn = document.getElementById("submitBtn")
+
+function saludar () {
+    console.log("El Bontón te Saluda");
+    alert("El Bontón te Saluda");
+}
